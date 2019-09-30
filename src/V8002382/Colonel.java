15fffb1c154ce428b48cae53527bbd5b70d62de6
gitGudOrGitOut <47678302+gitGudOrGitOut@users.noreply.github.com>
2019-09-30 18:23:25 +0100

    Finished Colonel class

---
+++ src/V8002382/Colonel.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package V8002382;
+
+/**
+ *
+ * @author v8002382
+ */
+public class Colonel extends Soldier implements Killable{
+
+    public Colonel(Faction faction) {
+        super(faction);
+        switch(faction)
+        {
+            case GDI:
+                points = 100;
+                break;
+            case NOD:
+                points = 95;
+                break;
+            case SCRIN:
+                points = 90;
+                break;
+        }
+    }
+
+    @Override
+    public String kill() {
+        return "A "+faction+" colonel retires, permanently: +"+points+"pts.\n";
+    }
+    
+}
